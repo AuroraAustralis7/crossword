2df7d84d4646219cb85546c99968b21ec23d8200
unknown <justinl611@nycstudents.net>
2026-06-14 20:09:27 -0400

    slight changes

--- crossword/client/src/App.tsx
+++ crossword/client/src/App.tsx
@@ -1,22 +1,101 @@
-// import styles from "./App.module.css";
-import { useState } from "react";
-
-type AppProps = {
-  puzzleSize: number;
-};
-
-function App(props: AppProps) {
-  const blankArray = [];
-  for (let i = 0; i < props.puzzleSize; i++) {
-    blankArray[i] = Array(props.puzzleSize);
-  }
-  const [crossword, setCrossword] = useState(blankArray);
-
-  return crossword.map((row, rowIndex) => {
-    crossword.map((col, colIndex) => {
-      <input type="text" maxLength={1} value="number" onChange={() => {}} />;
-    });
-  });
+type answerGridProps = 
+{
+    crossWordSize: number // 2x2? 3x3? 4x4? necessary to create the grid
+    words: string[] // list of words you want to place onto the grid
+    rows: number[] // list of rows corresponding to the position of the first letter in each word
+    cols: number[] // list of cols corresponding to the position of the first letter in each word
+    directions: string[] // list of directions corresponding to the position of the first letter in each word
+                         // note: directions can only be "up", "down", "left", or "right"
 }
 
-export default App;
+type appProps =
+{
+    answerGrid: string[]
+    gridProps: answerGridProps
+}
+
+// checks whether a single word lies entirely within the grid.
+function withinBounds(crossWordSize: number, words: string, row: number, col: number, direction: string)
+{
+    let lastRow = row;
+    let lastCol = col;
+    let withinBounds = true;
+
+    // this section gets the last rows and columns from the rows, columns and directions
+
+    for (let i = 0; i < words.length; i++)
+    {
+        if (direction = "up")
+        {
+            lastRow--;
+        }
+        if (direction = "down")
+        {
+            lastRow++;
+        }
+        if (direction = "left")
+        {
+            lastCol--;
+        }
+        if (direction = "right")
+        {
+            lastCol++;
+        }
+    }
+
+    // this section checks whether the first letter lies within the grid space
+
+    if (row >= crossWordSize || row < 0)
+    {
+        withinBounds = false;
+    }
+    if (col >= crossWordSize || col < 0)
+    {
+        withinBounds = false;
+    }
+
+    // this section checks whether the last letter lies within the grid space
+
+    if (lastRow >= crossWordSize || lastRow < 0)
+    {
+        withinBounds = false;
+    }
+    if (lastCol >= crossWordSize || lastCol < 0)
+    {
+        withinBounds = false;
+    }
+
+    return withinBounds;
+}
+
+function answerGrid(props: answerGridProps)
+{
+    const answerGrid = Array.from({ length: props.crossWordSize }, () => Array(props.crossWordSize).fill(""));
+
+    for (let i = 0; i < props.words.length; i++) // loop through each word in the list
+    {
+        if (withinBounds(props.crossWordSize, props.words[i], props.rows[i], props.cols[i], props.directions[i]))
+        {
+        for (let j = 0; j = props.words[i].length; j++) // loop through each character of the word
+        {
+            if (props.directions[i] === "up" )
+            {
+                answerGrid[props.rows[i - j]][props.cols[i]] = props.words[i][j];
+            }
+            if (props.directions[i] === "down" )
+            {
+                answerGrid[props.rows[i + j]][props.cols[i]] = props.words[i][j];
+            }
+            if (props.directions[i] === "left" )
+            {
+                answerGrid[props.rows[i]][props.cols[i - j]] = props.words[i][j];
+            }
+            if (props.directions[i] === "right" )
+            {
+                answerGrid[props.rows[i]][props.cols[i + j]] = props.words[i][j];
+            }
+        }
+    }
+    }
+    return answerGrid;
+}
--- crossword/client/src/main.tsx
+++ crossword/client/src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.tsx";
 
+type Direction = "left" | "right" | "up" | "down";
+const crosswordSize = 5;
+const wordsArray: string[] = ["Hi", "Bye", "Yes"];
+const rowsArray: number[] = [0, 1, 2]
+const colsArray: number[] = [0, 2, 2]
+const directionsArray: Direction[] = ["right", "down", "right"]
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 );
